6b21ed723a9d5d7951063ac40a114aafb2cd6b89
Tasha25 <greennatasha@aol.com>
2014-05-26 12:52:44 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsNoremediation::Application.config.secret_token = '2fe68bb92d1115f55471c27892a750e4bf61c6e8dad43251c879bcd9a5c9c16210d9c1162172c65831ad0d4497c82d5dd273724c6e5896aa5ee92f267c3272c2'
